278be3e0a1e6a2eca8320bf9bc810e050abba4bd
C-Hott <89225139+X-man34@users.noreply.github.com>
2024-03-27 08:57:08 -0700

    found traps for Mr. Buranik

--- src/main/java/org/tahomarobotics/scouting/scoutingserver/controller/MiscController.java
+++ src/main/java/org/tahomarobotics/scouting/scoutingserver/controller/MiscController.java
@@ -2,7 +2,16 @@ package org.tahomarobotics.scouting.scoutingserver.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.tahomarobotics.scouting.scoutingserver.util.APIUtil;
 import org.tahomarobotics.scouting.scoutingserver.util.Logging;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 public class MiscController {
 
@@ -13,5 +22,37 @@ public class MiscController {
         //for the selected team, go through all their matches and figure out who their alliance partners and opponents are
         //then for each match make a list of all teams to scout in that match. In order for them to need to be scouted, either their next match, or the one after that, they must be with us
         
+    }
+
+
+    //this is some code that mr buranik wanted to find all the matches that have had a trap done in the pnw so far.
+    //just leave it in as it may be useful
+    @FXML
+    public void findTraps(ActionEvent event) {
+        ArrayList<String> matchesWithTrap = new ArrayList<>();
+        try {
+            JSONArray data = APIUtil.get("/district/2024pnw/events/keys");
+            for (Object o : data.toList()) {
+                JSONArray eventData = APIUtil.get("/event/" + o.toString() + "/matches");
+                for (Object match : eventData) {
+                    JSONObject matchMap = (JSONObject) match;
+                    try {
+                        JSONObject breakDown = (JSONObject) matchMap.get("score_breakdown");
+                        JSONObject redBreakdown = (JSONObject) breakDown.get("red");
+                        JSONObject blueBreakdown = (JSONObject) breakDown.get("blue");
+
+                        if (redBreakdown.getBoolean("trapStageLeft") || redBreakdown.getBoolean("trapCenterStage") || redBreakdown.getBoolean("trapStageRight") || blueBreakdown.getBoolean("trapStageLeft") || blueBreakdown.getBoolean("trapCenterStage") || blueBreakdown.getBoolean("trapStageRight")) {
+                            matchesWithTrap.add(matchMap.get("key").toString());
+                        }
+                    }catch (Exception e) {
+                        //e.printStackTrace();
+                    }
+
+                }
+            }
+            System.out.println(matchesWithTrap);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
