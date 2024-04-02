de2cbac80d3d66bd4b35883a07700c2aaf679a03
C-Hott <89225139+X-man34@users.noreply.github.com>
2024-04-02 16:29:20 -0700

    Made changes to HTML tutoiral

--- resources/Tutorial/NavigationPanel.js
+++ resources/Tutorial/NavigationPanel.js
@@ -3,7 +3,7 @@ const template = document.createElement('template');
 template.innerHTML = `<div class="sidenav">
                               <a href="TutorialPage.html">About</a>
                               <a href="DataCollectionPage.html">Data Collection</a>
-                              <a href="#">Database Management</a>
+                              <a href="DatabaseManagement.html">Database Management</a>
                               <a href="#">Charts</a>
                           </div>`;
 
